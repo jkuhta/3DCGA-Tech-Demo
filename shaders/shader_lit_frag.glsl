93fc9a8f030e490f4066842965bff4f986f6dbe7
Darian Samsoedien <daj-samsoedien@live.nl>
2025-10-22 00:26:26 +0200

    refactored the file

--- shaders/shader_lit_frag.glsl
+++ shaders/shader_lit_frag.glsl
@@ -7,51 +7,54 @@ uniform vec3 lightPos, lightColor;
 uniform vec3 kd, ks;
 uniform float shininess;
 uniform vec3 viewPos;
-uniform bool useDiffuse;          // add diffuse term in specular modes
+uniform bool useDiffuse;
 uniform sampler2D colorMap;
 uniform bool hasTexCoords;
 uniform bool useMaterial;
-uniform int  shadingMode;         // 0=Default(unlit), 1=Lambert, 2=Phong, 3=Blinn
+uniform int  shadingMode;   // 0=Default, 1=Lambert, 2=Phong, 3=Blinn
 
 out vec4 outColor;
 
-vec3 albedo()
-{
+vec3 computeAlbedo() {
     return (hasTexCoords && !useMaterial) ? texture(colorMap, fragTexCoord).rgb : kd;
 }
 
+float lambertTerm(vec3 n, vec3 l) {
+    return max(dot(n, l), 0.0);
+}
+
+float phongSpecular(vec3 n, vec3 l, vec3 v, float shin) {
+    vec3 r = reflect(-l, n);
+    return pow(max(dot(r, v), 0.0), shin);
+}
+
+float blinnSpecular(vec3 n, vec3 l, vec3 v, float shin) {
+    vec3 h = normalize(l + v);
+    return pow(max(dot(n, h), 0.0), shin);
+}
+
 void main() {
-    vec3 N = normalize(fragNormal);
-    vec3 L = normalize(lightPos - fragPosition);
-    vec3 V = normalize(viewPos - fragPosition);
-    vec3 H = normalize(L + V);
-    float NdL = max(dot(N, L), 0.0);
+    vec3 normal  = normalize(fragNormal);
+    vec3 lightDir= normalize(lightPos - fragPosition);
+    vec3 viewDir = normalize(viewPos  - fragPosition);
 
-    vec3 C = vec3(0.0);
-    vec3 A = albedo();
+    float NdL = lambertTerm(normal, lightDir);
+    vec3  albedo = computeAlbedo();
 
-    if (shadingMode == 0) { // Default (unlit textured or kd)
-        C = A;
-    }
-    else if (shadingMode == 1) { // Lambert
-        C = A * NdL;
-    }
-    else if (shadingMode == 2) { // Phong
-        if (useDiffuse) C += A * NdL;
-        if (NdL > 0.0) {
-            vec3 R = reflect(-L, N);
-            float RdV = max(dot(R, V), 0.0);
-            C += ks * pow(RdV, shininess);
-        }
-    }
-    else { // 3 = Blinn-Phong
-        if (useDiffuse) C += A * NdL;
-        if (NdL > 0.0) {
-            float NdH = max(dot(N, H), 0.0);
-            C += ks * pow(NdH, shininess);
-        }
+    vec3 color = vec3(0.0);
+
+    if (shadingMode == 0) {                 // Default (unlit)
+        color = albedo;
+    } else if (shadingMode == 1) {          // Lambert
+        color = albedo * NdL;
+    } else if (shadingMode == 2) {          // Phong
+        if (useDiffuse) color += albedo * NdL;
+        if (NdL > 0.0)  color += ks * phongSpecular(normal, lightDir, viewDir, shininess);
+    } else {                                // Blinn-Phong
+        if (useDiffuse) color += albedo * NdL;
+        if (NdL > 0.0)  color += ks * blinnSpecular(normal, lightDir, viewDir, shininess);
     }
 
-    C *= lightColor;
-    outColor = vec4(C, 1.0);
+    color *= lightColor;
+    outColor = vec4(color, 1.0);
 }
